bb8875face1ddf5e9537bbfc6073a2453afb2956
Neil Patil <neilpatil@live.com>
2016-09-15 16:18:17 +0100

    Added Menu class with placeholder buttons and layout

---
+++ BattleGround/src/application/Menu.java
@@ -0,0 +1,60 @@
+package application;
+
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class Menu extends Stage {
+
+	Scene scene;
+	GridPane root;
+	Button start, stats, quit, options;
+
+	public Menu() {
+		Screen screen = Screen.getPrimary();
+		Rectangle2D bounds = screen.getVisualBounds();
+		setWidth(bounds.getWidth());
+		setHeight(bounds.getHeight());
+
+		start = new Button("START");
+		stats = new Button("STATS");
+		quit = new Button("QUIT");
+		options = new Button("OPTIONS");
+
+		start.setAlignment(Pos.CENTER);
+		stats.setAlignment(Pos.CENTER);
+		quit.setAlignment(Pos.CENTER);
+		options.setAlignment(Pos.CENTER);
+
+		start.setOnAction(e -> {
+
+		});
+
+		quit.setOnAction(e -> {
+			close();
+		});
+
+		options.setOnAction(e -> {
+
+		});
+
+		stats.setOnAction(e -> {
+
+		});
+
+		root = new GridPane();
+		root.setVgap(50);
+		root.setHgap(10);
+		scene = new Scene(root, getMaxWidth(), getMaxHeight());
+		root.add(start, 0, 0);
+		root.add(stats, 0, 1);
+		root.add(options, 0, 2);
+		root.add(quit, 0, 3);
+
+		setScene(scene);
+	}
+}
